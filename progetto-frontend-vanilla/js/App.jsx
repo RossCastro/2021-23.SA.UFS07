890f5b83e3711eac1e9e15c1b313989edcc52cf3
Ross <r.pensabene@itsrizzoli.it>
2022-05-31 12:36:15 +0200

    feat: f2

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,5 +1,5 @@
-import React from "test-1-ross";
-import {Button} from "test-1-ross/atoms/atoms/button/Button"
+import React from "react";
+import {Button} from "test-1-ross/atoms/button/Button"
 
 const App = () => ( 
     <div>
